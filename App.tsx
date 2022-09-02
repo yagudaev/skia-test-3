9ec183ec648e5894c23f816db99be49b5d70e6ed
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-09-02 18:39:58 +0200

    Try the hello world experiment

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
+import { Canvas, Circle, Group } from "@shopify/react-native-skia"
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
+  const size = 256
+  const r = size * 0.33
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ marginTop: 50 }}>Canvas Experiment</Text>
+      <Canvas style={{ flex: 1 }}>
+        <Group blendMode='multiply'>
+          <Circle cx={r} cy={r} r={r} color='cyan' />
+          <Circle cx={size - r} cy={r} r={r} color='magenta' />
+          <Circle cx={size / 2} cy={size - r} r={r} color='yellow' />
+        </Group>
+      </Canvas>
       <StatusBar style='auto' />
     </View>
   )
