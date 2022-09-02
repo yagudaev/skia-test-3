cd4f76d834ddb8f04e2ff31d67999927eff97f0d
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-09-03 01:29:25 +0200

    Try getting the web version working

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Canvas, Circle, Group } from "@shopify/react-native-skia"
+import { Box, Canvas, Circle, Group } from "@shopify/react-native-skia"
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 
@@ -9,6 +9,12 @@ export default function App() {
     <View style={styles.container}>
       <Text style={{ marginTop: 50 }}>Canvas Experiment</Text>
       <Canvas style={{ flex: 1 }}>
+        <Box
+          box={{ width: size, height: size, x: 1, y: 1 }}
+          color='red'
+          style='stroke'
+          strokeWidth={2}
+        />
         <Group blendMode='multiply'>
           <Circle cx={r} cy={r} r={r} color='cyan' />
           <Circle cx={size - r} cy={r} r={r} color='magenta' />
@@ -23,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
+    // alignItems: "center",
+    // justifyContent: "center"
   }
 })
